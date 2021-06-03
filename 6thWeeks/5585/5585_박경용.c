55870533984bc3343a9014d44b9c3e914fb785ad
Roddyd94 <pky941129@gmail.com>
2021-06-03 15:46:17 +0900

    5585 solved 박경용

---
+++ 6thWeeks/5585/5585_박경용.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int coin[6] = { 500,100,50,10,5,1 };
+
+int main(void)
+{
+	int money = 0, count = 0;
+	int index = 0;
+
+	scanf("%d", &money);
+	money = 1000 - money;
+
+	while (money > 0)
+	{
+		if (money >= coin[index])
+		{
+			money -= coin[index];
+			++count;
+		}
+		else if (index < 6)
+		{
+			++index;
+		}
+	}
+
+	printf("%d", count);
+
+	return 0;
+}
